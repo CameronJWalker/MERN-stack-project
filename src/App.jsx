39db92c9c9e580f37afbf483da1eec03f7715897
CameronJWalker <cameron.jr.walker@icloud.com>
2022-09-14 18:59:41 -0700

    added routes and linked buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
 import './App.css';
-import Nutrition  from '/components/nutrition';
-import newWorkout from './components/newWorkout';
-import prLog from './components/prlog'
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nutrition  from './components/nutrition';
+import NewWorkout from './components/newWorkout.jsx';
+import PRLog from './components/PRLog'
+import Home from './components/Home.jsx';
 
 export default function App() {
      return (
       <>
-      < newWorkout />
-      < Nutrition />
-      < prLog />
-      <button> New Workout </button>
-      <button> New Nutrition </button>
-      <button> New Personal Record </button>
-        <div className="App">
+       <div className="App">
           <header className="App-header">
-           <p>
-               Workout App
-          </p>
-    </header>
-   </div>
-   </>
+            <nav>
+              Workout App
+            </nav>
+          </header>
+        </div>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/newworkout" element = {<NewWorkout/>}/>
+        <Route path = "/nutrition" element = {<Nutrition/>}/>
+        <Route path = "/prlog" element = {<PRLog/>}/>
+
+      </Routes>
+      </>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default function Home() {
+    return (
+        <>
+          <a href = '/newWorkout'><button> New Workout </button></a>
+          <a href = '/nutrition'><button> New Nutrition </button></a>
+          <a href = '/prlog'><button> New Personal Record </button></a>
+        </>
+    )
+    }
